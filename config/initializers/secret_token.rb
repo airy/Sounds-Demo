4aacbf0582f9dcad0ce99647720ca7fe4f99ff1c
airy <airydragon@gmail.com>
2010-06-29 23:55:35 +0300

    rails (3.0.0.beta4) project initialized.
    $ rails new sounds -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '122c166945e147db5b944a8a4e90c5fc7da0b56293e1153e44e8f7a7529aa8e4ac23f0344161debc26fc005618077609c49e22b453df0a9eb74c227d04bfcf37'
