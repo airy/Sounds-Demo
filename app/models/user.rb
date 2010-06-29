c2a17601e9aa547bd0ad06c6a76f97560a9e1dea
airy <airydragon@gmail.com>
2010-06-30 00:22:32 +0300

    username added to user model & views.
    * validation for username added to user model
    * to_s method overrided.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :username
+  
+  validates :username, :presence => true, :length => {:minimum => 4},
+    :uniqueness => {:case_sensitive => false}, :format => { :with => /[A-Za-z0-9]+/ }
+  
+  # it's one nice handy shortcut for displaying username
+  def to_s
+    username
+  end
+  
 end
